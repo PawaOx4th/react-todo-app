ae48aa2d2acbd8ca239eb2806681a524e0a58fe4
pawaox4th <pawaox4th@gmail.com>
2022-09-03 16:39:34 +0700

    feat: 🎸 Update content on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { BsGithub } from "react-icons/bs"
+import { FOOTER } from "../utils/constant"
 
 type Props = {}
 
 function Footer({}: Props) {
-  return <footer>Footer</footer>
+  return (
+    <footer className="flex justify-end bg-dark p-3 text-mid">
+      <a href={FOOTER.GITHUB} target={"_blank"}>
+        <div className="flex items-center gap-3">
+          <BsGithub />
+          <strong>{FOOTER.PROJECT_NAME}</strong>
+        </div>
+      </a>
+    </footer>
+  )
 }
 
 export default Footer
---
+++ src/utils/constant.ts
@@ -0,0 +1,9 @@
+export const FOOTER = {
+  GITHUB: "https://github.com/PawaOx4th/react-todo-app",
+  PROJECT_NAME: "PawaOx4th/react-todo-app"
+}
+
+const CONSTANT = {
+  FOOTER
+}
+export default CONSTANT
